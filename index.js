33ae63868568e06b43b8db60465cac39e4c2ff8d
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-15 09:39:04 -0600

    get

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+import {firestore as db}  from './firebase' 
+import firebase from './firebase';
 
 var app = express();
 
@@ -16,6 +18,32 @@ app.all("/", function (req, res, next) {
   next();
 });
 //end of global
+
+//db
+
+const getTest = async function(){
+  let list = await getDB("test")
+  console.log(list)
+}
+
+const getDB = async function(col){
+  const ram = db.collection(col);
+  console.log(col)
+  console.log("uwu")
+  const get = await ram.get().then((querySnapshot) => {
+    return querySnapshot
+  })
+  const list = get.docs.map(doc => doc.data());
+  return list
+}
+//end of db
+
+//db get
+app.get("/get", async (req, res, next) => {
+  var colName = req.query.name
+  var list = await getDB(colName)
+  res.json({data:list});
+});
 
 //test
 app.get("/", async (req, res, next) => {
