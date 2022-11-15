40fa131a1abe154387112261c25af9d01c530131
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-15 12:42:51 -0600

    newid

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get("/new", async (req, res, next) => {
 const newID = async function(){
   let id = "a"
   let ramReg = await getReg("test", id)
-  while(ramReg==""){
+  while(ramReg!=""){
     id=await fetch("https://securitypassword.cyclic.app/generate/?low=true&up=true&n=false&num=false&char=false&rect=false&len=10", {method : 'GET',})
     .then(function(response) {
        return response.json(); })
